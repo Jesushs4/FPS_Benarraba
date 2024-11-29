97bbefb1c5a899c22a9a7fe75cfb33057a91d544
Jesús Herrera <115071141+Jesushs4@users.noreply.github.com>
2024-11-29 19:46:00 +0100

    ladder climbable

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class Ladder : MonoBehaviour
 {
 
+    [SerializeField] private BoxCollider climbArea;
+
+
     public void PutLadder(Transform placeholderTransform)
     {
         
             transform.position = placeholderTransform.position;
             transform.rotation = placeholderTransform.rotation;
             gameObject.layer = LayerMask.GetMask("Default");
+            climbArea.enabled = true;
     }
 
 
