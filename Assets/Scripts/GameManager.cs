4fade540e8d20acbd0fd6fb5a78558e260d2ed55
Jesús Herrera <115071141+Jesushs4@users.noreply.github.com>
2024-12-09 19:16:27 +0100

    game over panel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,6 +20,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject hudPanel;
     private TextMeshProUGUI sheepCounterText;
     private TextMeshProUGUI timerText;
+    [SerializeField] private GameObject endPanel;
+    [SerializeField] private TextMeshProUGUI endTime;
 
 
     private void Awake()
@@ -43,11 +45,16 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        sheepCounterText.text = "Sheeps left: " + sheepCounter;
+        sheepCounterText.text = "Ovejas restantes: " + sheepCounter;
 
         if (!IsPaused)
         {
             UpdateTimer();
+        }
+
+        if (sheepCounter <= 0)
+        {
+            EndGame();
         }
     }
 
@@ -59,12 +66,25 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void EndGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+        hudPanel.SetActive(false);
+        endPanel.SetActive(true);
+        int minutes = Mathf.FloorToInt(elapsedTime / 60f);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60f);
+        endTime.text = "Tiempo: " + $"{minutes:00}:{seconds:00}";
+        
+
+    }
+
     private void UpdateTimer()
     {
         elapsedTime += Time.deltaTime;
         int minutes = Mathf.FloorToInt(elapsedTime / 60f);
         int seconds = Mathf.FloorToInt(elapsedTime % 60f);
-        timerText.text = "Time: "+$"{minutes:00}:{seconds:00}";
+        timerText.text = "Tiempo: "+$"{minutes:00}:{seconds:00}";
     }
 
     public void TogglePause()
@@ -88,5 +108,10 @@ public class GameManager : MonoBehaviour
     public void ReturnToMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("OutdoorsScene");
     }
 }
